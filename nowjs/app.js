acd117b6318708e137b2c488739343dbd59e58b5
SharangP <sharang.phadke@gmail.com>
2012-10-07 17:32:44 -0400

    mpd-scripts, funcional nowjs

--- nowjs/app.js
+++ nowjs/app.js
@@ -1,44 +1,85 @@
-var html = require('fs').readFileSync(__dirname+'/helloworld.html');
-var server = require('http').createServer(function(req, res){
-  res.end(html);
-});
-server.listen(8080);
-var sys = require('sys');
-var exec = require('child_process').exec;
-var mpdSocket = require('mpdsocket');
-var mpd = new mpdSocket('localhost','6600');
+/*
+ * NebulUs
+ */
 
-	mpd.send('play',function(r) {
-		console.log(r);
+/*
+ * Dependency variables
+ */
+    var sys = require('sys');
+    var exec = require('child_process').exec;
+    var mpdSocket = require('mpdsocket');
+    var mpd = new mpdSocket('localhost','6600');
+    var nowjs = require("now");
+
+/*
+ * Create server
+ */
+    var html = require('fs').readFileSync(__dirname+'/helloworld.html');
+    var server = require('http').createServer(function(req, res){
+        res.end(html);
     });
+    server.listen(8080);
 
-function puts(error, stdout, stderr) { sys.puts(stdout) }
-function putsval(error, stdout, stderr) { sys.puts(stderr); statusv=stdout}
-var nowjs = require("now");
-var everyone = nowjs.initialize(server);
-var statusv;
- // Server side
- everyone.now.checkValue = function() {
-      console.log(this.now.command); 
-      if(this.now.command=='play') {
-		exec("/home/pi/nebulus/play.py",puts);
+/*
+ *  Variable declarations
+ */
+    var everyone = nowjs.initialize(server);
+    var statusv= 'pause';
+
+/*
+ * Functions
+ */
+    //Send command to mpd, log output to console
+    function cSend(cmd) {
+	    mpd.send(cmd,function(r) {
+		    console.log(r);
+	    });
+    }
+
+    //Logs output of exec to stdout (command line)
+    function puts(error, stdout, stderr) { sys.puts(stdout) }
+    
+    //Similar to above. Logs to stderr and sets statusv to val of stdout
+    function putsval(error, stdout, stderr) { sys.puts(stderr); statusv=stdout}
+ 
+    // Server side logic to check current state of mpd
+    everyone.now.checkValue = function() {
+	    console.log(this.now.command);
+	    cSend('status');
+        
+        if(this.now.command=='play') {
+		    cSend('play');
 		}
-      else if(this.now.command=='pause') {
-		exec("/home/pi/nebulus/pause.py",puts)
+        else if(this.now.command=='pause') {
+		    cSend('pause');
 		}
-      else if(this.now.command=='prev') {
-		exec("/home/pi/nebulus/prev.py",puts);
+        else if(this.now.command=='prev') {
+		    if( this.now.status=='play') {
+			    cSend('previous');		
+		    }
+		    else {
+			    cSend('previous')
+			    cSend('pause');
+		    }		
 		}
-      else if(this.now.command=='next') {
-		exec("/home/pi/nebulus/next.py",puts);
+        else if(this.now.command=='next') {
+		    if( this.now.status=='play') {
+			    cSend('next');		
+		    }
+		    else {
+			    cSend('next')
+			    cSend('pause');
+		    }		
 		}
-	
-	everyone.now.setStatus(this.now.status)
-	statusv=this.now.status;
- }
-everyone.now.askStatus= function() {
-	this.now.setStatus(statusv);
-}
+	   
+        everyone.now.setStatus(this.now.status);
+        statusv=this.now.status;
+    }
+      
+    //Sets initial status?  
+    everyone.now.askStatus= function() {
+	    this.now.setStatus('bitch');
+    }   
 	
 
 
